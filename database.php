296007c7d33074606bf63706d229a49678b58805
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-11-29 21:03:52 +0000

    Querying the Database
    How to set up our database connection to be more helpful and query the
    database

--- database.php
+++ database.php
@@ -2,10 +2,18 @@
 
 try {
 	$db = new PDO("mysql:host=localhost;dbname=shirts4mike;","root","root");
-	var_dump($db);
+	// var_dump($db);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->exec("SET NAMES 'utf8'");
 } catch (Exception $e) {
 	echo "Could not connect to the database.";
 	exit;
 }
 
-echo "Woo-hoo!";
+try {
+	$results = $db->query("SELECT name, price FROM products ORDER BY sku ASC");
+	echo "query ran successfully!";
+} catch (Exception $e) {
+	echo "data could not be retrieved from database";
+	exit;
+}
